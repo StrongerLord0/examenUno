bce811a542a0bda3ff73c0762a248efe589ea38e
Misael <Cecilia@DESKTOP-6NT86PH>
2021-04-23 03:16:15 -0600

    Este es el examen del equipo uno en Github

--- src/examenequipouno/ExamenEquipoUno.java
+++ src/examenequipouno/ExamenEquipoUno.java
@@ -1,21 +1,15 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Programacion Orientada a Objetos
+ * Examen Equipo 1 | Unidad 2 
+ * Docente: Juana Garcia Saenz
  */
+
 package examenequipouno;
 
-/**
- *
- * @author Cecilia
- */
 public class ExamenEquipoUno {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
     }
     
 }
---
+++ src/examenequipouno/Resta.java
@@ -0,0 +1,6 @@
+
+package examenequipouno;
+
+public class Resta {
+    
+}
